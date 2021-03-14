60df8b7f23713bdd166005f424ac468528ae0ac3
Italo Evan <igbehh@gmail.com>
2021-03-14 20:26:52 -0300

    UserModel added

--- lib/data/models/posts_model.dart
+++ lib/data/models/posts_model.dart
@@ -4,6 +4,7 @@ class PostsModel {
   String user_name;
   String user_id;
   String user_image;
+  String date;
 
   PostsModel.fromJson(Map<String, dynamic> map) {
     tag = map['tag'];
@@ -11,6 +12,7 @@ class PostsModel {
     user_name = map['user_name'];
     user_id = map['user_id'];
     user_image = map['user_image'];
+    date = map['date'];
   }
 
   Map<String, dynamic> toJson() {
@@ -20,6 +22,7 @@ class PostsModel {
     map['user_name'] = user_name;
     map['user_id'] = user_id;
     map['user_image'] = user_image;
+    map['date'] = date;
     return map;
   }
 }
---
+++ lib/data/models/user_model.dart
@@ -0,0 +1,23 @@
+class UserModel {
+  String name;
+  int n_posts;
+  String user_image;
+  int user_id;
+
+  UserModel.fromJson(Map<String, dynamic> map) {
+    name = map['name'];
+    n_posts = map['n_posts'];
+    user_image = map['user_image'];
+    user_id = map['user_id'];
+  }
+
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> map = Map();
+
+    map['name'] = name;
+    map['n_posts'] = n_posts;
+    map['user_image'] = user_image;
+    map['user_id'] = user_id;
+    return map;
+  }
+}
